482da3f527163030de6f3231467b956d29d8b61f
letudk <tule.itc@gmail.com>
2023-09-27 08:26:22 +0700

    add base code  have Extention and MUI, MUI ICON

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Button from '@mui/material/Button'
+import { AccessAlarm, ThreeDRotation, OtherHouses } from '@mui/icons-material'
+
+function App() {
+  return ( <>
+    <h1>Hello</h1>
+    <Button variant='contained'>Hello world</Button>
+    <Button variant='outlined'>OutLine</Button>
+    <AccessAlarm/>
+    <ThreeDRotation />
+    <OtherHouses />
+  </>
+  )
+}
+
+export default App
