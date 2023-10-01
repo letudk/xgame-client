b4d1ed18553364b8cd535537a7c0a8761a103f93
letudk <tule.itc@gmail.com>
2023-10-01 22:13:49 +0700

    add dark mode, light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Button from '@mui/material/Button'
 import { AccessAlarm, ThreeDRotation, OtherHouses } from '@mui/icons-material'
+import { Typography } from '@mui/material'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return ( <>
+    <ModeToggle/> <hr></hr>
     <h1>Hello</h1>
     <Button variant='contained'>Hello world</Button>
     <Button variant='outlined'>OutLine</Button>
+    <Typography variant='body' >TEST TYPOGRAPHY</Typography>
     <AccessAlarm/>
     <ThreeDRotation />
     <OtherHouses />
