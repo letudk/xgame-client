b4ac950009215a9812f88a2a6f091bc40da1335a
letudk <tule.itc@gmail.com>
2023-10-02 22:33:59 +0700

    add learn dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import Button from '@mui/material/Button'
+import * as React from 'react'
+import {Button, FormControl, InputLabel, MenuItem} from '@mui/material'
+import Select from '@mui/material/Select'
 import { AccessAlarm, ThreeDRotation, OtherHouses } from '@mui/icons-material'
 import { Typography } from '@mui/material'
 import { useColorScheme } from '@mui/material/styles'
+
+function SelectMode() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectModeValue = event.target.value
+    setMode(selectModeValue)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="select-small-label">Mode</InputLabel>
+      <Select
+        labelId="select-small-label"
+        id="select-small"
+        value={mode}
+        label="Mode"
+        onChange={handleChange} >
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value= "system">System</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
@@ -18,6 +45,8 @@ function ModeToggle() {
 
 function App() {
   return ( <>
+    <SelectMode/>
+    <hr></hr>
     <ModeToggle/> <hr></hr>
     <h1>Hello</h1>
     <Button variant='contained'>Hello world</Button>
